67c5691357ba47b2be1a14a95473dfd89a33e8bc
Cristian Saavedra Desmoineaux <cristian.saavedra.desmoineaux@gmail.com>
2015-01-25 19:47:11 -0500

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#The first function, `makeCacheMatrix` creates a special "matrix", which is
+#really a list containing a function to
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  print(environment())
+  evn <- environment()
+  print(parent.env(evn))
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  getevn<- function() environment()
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve,
+       getevn = getevn)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
